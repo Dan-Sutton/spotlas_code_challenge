88165a669782eb69eb179a3dfff230f28f5e7c42
Dan-Sutton <dewasutton@gmail.com>
2023-01-11 14:26:51 +0000

    feed single container

--- lib/pages/feed.dart
+++ lib/pages/feed.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:spotlas_code_challenge/components/post.dart';
 
 class Feed extends StatefulWidget {
   const Feed({Key? key}) : super(key: key);
@@ -10,6 +11,21 @@ class Feed extends StatefulWidget {
 class _FeedState extends State<Feed> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: Colors.transparent,
+        title: Text(
+          'Feed',
+          style: TextStyle(color: Colors.black),
+        ),
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Post(),
+        ],
+      ),
+    );
   }
 }
