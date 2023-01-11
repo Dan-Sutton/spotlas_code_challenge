29e77f0bf6cffd7e073fcbe8da1fced8dd58be9d
Dan-Sutton <dewasutton@gmail.com>
2023-01-11 13:31:06 +0000

    layout guides

---
+++ lib/styles/layout_styles.dart
@@ -0,0 +1,41 @@
+Map smallLayout = {
+  'xxxxs': 1,
+  'xxxs': 1,
+  'xxs': 2,
+  'xs': 4,
+  's': 8,
+  'm': 12,
+  'l': 16,
+  'xl': 24,
+  'xxl': 32,
+  'xxxl': 48,
+  'xxxxl': 96,
+};
+
+Map mediumLayout = {
+  'xxxxs': 1,
+  'xxxs': 2,
+  'xxs': 4,
+  'xs': 8,
+  's': 12,
+  'm': 16,
+  'l': 24,
+  'xl': 32,
+  'xxl': 48,
+  'xxxl': 64,
+  'xxxxl': 128,
+};
+
+Map largeLayout = {
+  'xxxxs': 1,
+  'xxxs': 3,
+  'xxs': 6,
+  'xs': 12,
+  's': 16,
+  'm': 24,
+  'l': 32,
+  'xl': 40,
+  'xxl': 56,
+  'xxxl': 80,
+  'xxxxl': 160,
+};
